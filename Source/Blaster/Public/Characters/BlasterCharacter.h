d1d7a0902791d3c0ce021e1d0536ce90bafe7510
김경수 <galtab0403@gmail.com>
2024-03-13 10:18:24 +0900

    Set Character Movement

--- Source/Blaster/Public/Characters/BlasterCharacter.h
+++ Source/Blaster/Public/Characters/BlasterCharacter.h
@@ -12,18 +12,24 @@ class BLASTER_API ABlasterCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	ABlasterCharacter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
+	
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+
+protected:
+	virtual void BeginPlay() override;
+	
+	void MoveForward(float Value);
+    void MoveRight(float Value);
+    void Turn(float Value);
+    void LookUp(float Value);
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class UCameraComponent* FollowCamera;
 };
