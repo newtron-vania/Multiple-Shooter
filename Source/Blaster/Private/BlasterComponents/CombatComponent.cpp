e191d8e2401bfa3fc6f3cdb7c341f5fb25f36e93
김경수 <galtab0403@gmail.com>
2024-06-13 22:08:37 +0900

    Implement Spawing Projectile

--- Source/Blaster/Private/BlasterComponents/CombatComponent.cpp
+++ Source/Blaster/Private/BlasterComponents/CombatComponent.cpp
@@ -109,9 +109,11 @@ void UCombatComponent::TraceUnderCrosshairs(FHitResult& TraceHitResult)
 		if (!TraceHitResult.bBlockingHit)
 		{
 			TraceHitResult.ImpactPoint = End;
+			HitTarget = End;
 		}
 		else
 		{
+			HitTarget = TraceHitResult.ImpactPoint;
 			DrawDebugSphere(
 				GetWorld(),
 				TraceHitResult.ImpactPoint,
@@ -135,7 +137,7 @@ void UCombatComponent::MulticastFire_Implementation()
 	if (Character)
 	{
 		Character->PlayFireMontage(bAiming);
-		EquippedWeapon->Fire();
+		EquippedWeapon->Fire(HitTarget);
 	}
 }
 
